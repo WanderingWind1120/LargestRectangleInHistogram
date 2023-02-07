d2da38e11409fc455c8b95a1b9d78809de3ef2d4
WanderingWind1120 <121502769+WanderingWind1120@users.noreply.github.com>
2023-02-07 09:15:36 +0700

    Update LargestRectangleInHistogram.java

--- LargestRectangleInHistogram.java
+++ LargestRectangleInHistogram.java
@@ -22,7 +22,16 @@ public class LargestRectangleInHistogram {
             int p = i+1;
 
             while ( p <= height.length -1 && height[p] >= height[i]){
-                p = lessthanright[p];
+                p = lessthanright[p];// Cụ thể bản chất thuật toán này nếu viết tường minh ra không cần tối ưu về thời gian chạy và bộ nhớ
+                // thì cái lessthanright ở vị trí cột hiện tại chính là tìm xem thứ tự cột bên phải xa nhất liên tiếp mà vẫn có độ cao lớn hơn 
+                // cột hiện tại 
+                // trong thuật toán tường minh thì cái while loop này mới thực sự có ý nghĩa 
+                // Còn đây là dạng tối ưu cho thời gian chạy khi mà nó sẽ lấy kết quá của array lessthanright của cột ngay sau nó 
+                // vì khi viết kiểu này nó sẽ giống với cái kiểu dùng một kết quả tính toán trước đó nên thực ra nó không sự tính toán 
+                // nên chỗ này thậm trí còn dùng if statement được 
+                // còn việc tại sao phải sử dụng p làm biến trung gian mà không sử dụng biến khác được 
+                // vì khi cái while statement này không xảy ra thì mặc định cái giá trị của array lessthanright ở vị trí hiện tại sẽ có giá trị bằng chính nó là 
+                // i và p (vì cái array này nó tìm số thực tự cột trong array height chứ không tìm giá trị 
             }
             lessthanright [i] = p;
         }
